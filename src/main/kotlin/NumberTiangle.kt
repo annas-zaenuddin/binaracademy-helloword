19af0afbf25e322b61216a597b25a9e716f05517
ANNAS\asus <annaszaenuddin@gmail.com>
2021-04-26 22:45:35 +0700

    creating feature number triangle

---
+++ src/main/kotlin/NumberTiangle.kt
@@ -0,0 +1,11 @@
+class NumberTiangle {
+    fun createNumberTriangle(maxNumber: Int) {
+        for (i in 1..maxNumber) {
+            for (j in 1 ..i) {
+                println(j)
+            }
+            println()
+
+        }
+    }
+}
